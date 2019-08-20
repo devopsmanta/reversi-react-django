bd8e0bb492cd272592a0dd75090a80e70a1c38df
Melvin Löfgren <devopsmanta@gmail.com>
2019-08-20 15:18:58 +0800

    implement get_token

--- reversiapi/views.py
+++ reversiapi/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -6,17 +7,23 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from .serializers import ResultSerializer, LoginSerializer, RegisterSerializer, ProfileSerializer, ModifySerializer
 
 
+def get_token(request):
+    token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[0]
+    data = {'token': token}
+    return data
+
+
 class LoginApiView(generics.GenericAPIView):
     serializer_class = LoginSerializer
 
-    def post(self, request, *args, **kwargs):
+    def post(self, request):
         serializer = self.get_serializer(data=request.data)
         try:
             serializer.is_valid(raise_exception=True)
         except ValidationError as e:
             return Response({
                 "message": e.detail['message'][0]
-            }, 400)
+            }, status.HTTP_400_BAD_REQUEST)
         user = serializer.validated_data
         refresh = RefreshToken.for_user(user)
 
@@ -43,9 +50,8 @@ class ProfileApiView(generics.GenericAPIView):
     serializer_class = ProfileSerializer
 
     def get(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[0]
-        data = {'token': token}
-        serializer = self.get_serializer(data=data)
+        token = get_token(request)
+        serializer = self.get_serializer(data=token)
         serializer.is_valid(raise_exception=False)
         user = serializer.validated_data
 
@@ -56,9 +62,8 @@ class ModifyApiView(generics.GenericAPIView):
     serializer_class = ModifySerializer
 
     def post(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[0]
-        data = {'token': token}
-        profile_serializer = ProfileSerializer(data=data)
+        token = get_token(request)
+        profile_serializer = ProfileSerializer(data=token)
         profile_serializer.is_valid(raise_exception=True)
         user = profile_serializer.validated_data
 
@@ -76,9 +81,8 @@ class ResultApiView(generics.GenericAPIView):
     serializer_class = ResultSerializer
 
     def get(self, request):
-        token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[0]
-        data = {'token': token}
-        profile_serializer = ProfileSerializer(data=data)
+        token = get_token(request)
+        profile_serializer = ProfileSerializer(data=token)
         profile_serializer.is_valid(raise_exception=True)
         user = profile_serializer.validated_data
         serializer = self.get_serializer(data=user.result_set.all(), many=True)
@@ -90,11 +94,8 @@ class ResultApiView(generics.GenericAPIView):
         return Response(serializer.data)
 
     def post(self, request):
-        print(request.data.get('finalTime'))
-
-        token = request.META.get('HTTP_AUTHORIZATION', " ").split(' ')[0]
-        data = {'token': token}
-        profile_serializer = ProfileSerializer(data=data)
+        token = get_token(request)
+        profile_serializer = ProfileSerializer(data=token)
         profile_serializer.is_valid(raise_exception=True)
         user = profile_serializer.validated_data
         serializer = self.get_serializer(data=request.data)
